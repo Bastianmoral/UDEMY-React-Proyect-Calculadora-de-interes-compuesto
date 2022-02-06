2e4dbdc50f7a23d26e9a553d05c939ff8164cd2e
bastianmoral <bastihc@gmail.com>
2022-02-05 22:20:52 -0300

    Seccion 13 udemy react Proyecto calculadora de interés compuesto

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useState} from 'react'
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup'
+import Input from './components/Input'
+import Button from './components/Button'
+import Container from './components/Container'
+import Section from './components/Section'
+import Balance from './components/Balance'
+
+const compoundInterest = ( deposit, contribution, years, rate) => {
+  let total = deposit
+  for ( let i=0; i < years; i++) {
+    total = (total + contribution) * (rate + 1)
+  }
+  return Math.round(total)
+} 
+
+
+
+const formatter = new Intl.NumberFormat( 'en-US', {
+  styled: 'currency',
+  currency: 'USD',
+  minimumFractionDigits: 2,
+  maximumFractionDigits: 2,
+})
+function App() {
+  const [balance, setBalance] = useState('')
+  const handleSubmit = ({ deposit, contribution, years, rate } ) => {
+    const val = compoundInterest(Number(deposit), Number(contribution), Number(years), Number(rate))
+    setBalance(formatter.format (val))
+  }
+
+  
+  return (
+    <Container>
+      <Section> 
+        <Formik
+          initialValues={{
+            deposit: '',
+            contribution: '',
+            years: '',
+            rate: '',
+          }}
+          onSubmit={handleSubmit}
+          validationSchema={Yup.object( { 
+            deposit: Yup.number().required('Campo obligatorio').typeError('Debe ingresar solo numeros'), //objeto de configuración
+            contribution: Yup.number().required('Campo obligatorio').typeError('Debe ingresar solo numeros'), 
+            years: Yup.number().required('Campo obligatorio').typeError('Debe ingresar solo numeros'), 
+            rate: Yup.number().required('Campo obligatorio').typeError('Debe ingresar solo numeros').min(0, 'El valor mínimo es 0').max(1, 'El valor máximo es 1'), 
+          })}
+        >
+          <Form>
+            <Input name='deposit' label='Depósito inicial'/>         
+            <Input name='contribution' label='Contribución Anual' />         
+            <Input name='years' label='Años' />         
+            <Input name='rate' label='Interes Estimado'/>
+            <Button type = 'submit'>Calcular</Button>         
+          </Form>
+
+        </Formik>
+        {balance !== '' ? <Balance>Balance final: {balance}</Balance> : null}
+      </Section>
+    </Container>
+  )
+}
+
+export default App;
